62ebceb1c9469f4b70519f1ef7535310c96a2b11
ukulya <ukulya150992@gmail.com>
2021-11-18 13:12:41 +0600

    fix transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-
+import { TransitionGroup,CSSTransition } from 'react-transition-group';
 import About from './components/About'
 import Home from './components/Home'
-import { BrowserRouter as Router, Routes, Route, Link as NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 import NotFound from './components/NotFound'
 import Details from './components/Details'
 
-const App = () => {
- 
+const routes = [
+  { path: '/', name: 'Home', element: Home },
+  { path: '/about', name: 'About', element: About },
+  { path: '/details/:id', name: 'Details', element: Details },
+  { path: '*', name: 'NotFound', element: NotFound },
+]
 
+const App = ({ location }) => {
   return<Router>
     <div>
       <nav>
@@ -21,12 +26,16 @@ const App = () => {
           </li>
         </ul>
       </nav>
-      <Routes>
+      {/* <TransitionGroup> */}
+      <CSSTransition /* key={location.pathname.split('/')[1] || '/'} */ timeout={300} classNames="fading-animation-transition" appear >
+      <Routes /* location={location} */>
         <Route path="/about" element={<About />}/>
         <Route path="/" element={<Home/>}/>
         <Route path="/details/:id" element={<Details/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
+      </CSSTransition>
+      {/* </TransitionGroup> */}
     </div>
   </Router>
 }
